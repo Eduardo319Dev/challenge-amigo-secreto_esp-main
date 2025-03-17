6300b0c50a6995a5c44bdde1a4f335d33911598b
Eduardo <eduardopachecodelangel@gmail.com>
2025-03-17 14:32:04 -0500

    new funciones

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -15,6 +15,31 @@ function agregarAmigo() {
 
   //limpia el campo de entrada 
   document.getElementById("amigo").value = "";
+  actualizarLista(); // llamar ala funcion
 
+}
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("no hay amigos en la lista para sortear.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigosSorteado = amigos[indiceAleatorio];
+
+    document.getElementById("resultado").innerHTML = `<li>${amigosSorteado}</li>`;
 }
 
